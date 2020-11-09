0d91d2de1d6312cc9fd1af3b27b1bad9e153ca2e
Kyle Hoffpauir <khoffpau@highpoint.edu>
2020-11-09 12:45:48 -0500

    java prog outline

---
+++ JavaSort.java
@@ -0,0 +1,16 @@
+/*
+  Authors: Nathan Markle and Kyle Hoffpauir
+  Date: 11/10/2020
+  Class: CSC-4510
+  Description:
+
+*/
+
+public class JavaSort {
+    public static void main(String[] args) {
+  }
+
+    static void Sort(Object paramObj) {
+    }
+
+}
