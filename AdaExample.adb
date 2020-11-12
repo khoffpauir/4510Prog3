2f9ce834bb3507108c36d116401ffb22581b046b
Kyle Hoffpauir <khoffpau@highpoint.edu>
2020-11-12 17:07:45 -0500

    example

---
+++ AdaExample.adb
@@ -0,0 +1,25 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+
+procedure AdaExample is
+  generic
+    type Item is private; --this is a generic parameter
+  procedure Swap(X, Y: in out Item) is
+    T: Item;
+  begin
+    T := X;
+    X := Y;
+    Y := T;
+  end Swap;
+-- an instantiation, which provide the actual generic parameter.
+procedure Swap_Float is new Swap(Float);
+-- Swap_Float can now be called..
+a, b: float;
+
+begin
+    a := 10;
+    b := 20;
+    Swap_Float (a, b);
+    --Note the parameters here
+    --match the generic specification
+end AdaExample;
--- AdaSort.adb
+++ AdaSort.adb
@@ -1,7 +1,7 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
-procedure AdaPartA is
+procedure AdaSort is
   x : Integer;
   y : Integer;
   z : Integer;
@@ -12,4 +12,4 @@ begin
    Get (y);
    z := x + y;
    Put_Line(Integer'Image (x) & " + " & Integer'Image (y) & " = " & Integer'Image (z));
-end AdaPartA;
+end AdaSort;
